8b09e215e42c0ac99c9550160e6832aea4a9d8ea
mapi <rfriends2017@ymail.ne.jp>
2025-01-05 04:36:26 +0900

    Update rfriends3_arch.sh

--- rfriends3_arch.sh
+++ rfriends3_arch.sh
@@ -1,91 +1,20 @@
 #!/bin/sh
 # -----------------------------------------
 # Rfriends (radiko radiru録音ツール)
-# 2024/12/16 new github
-# -----------------------------------------
-SITE=https://github.com/rfriends/rfriends3/releases/latest/download
-SCRIPT=rfriends3_latest_script.zip
-dir=.
-user=`whoami`
-HOME=/home/$user
-userstr="s/rfriendsuser/${user}/g"
-# -----------------------------------------
-echo
-echo rfriends3 Setup Utility Ver. 1.0
-echo
-echo "これは Arch Linux 用です"
+# 1.0 2024/12/16 new github
+# 1.1 2025/01/05 fix
+ver=1.1
+echo start $ver
 echo
 # -----------------------------------------
-# ツールのインストール
-# -----------------------------------------
-#sudo ln -sf /usr/share/zoneinfo/Asia/Tokyo /etc/localtime
-
-# update
-sudo pacman -Syu
-
-echo
-echo php, ffmpeg, at, AtomicParsley
-echo
-
-echo "上記ツールをインストールしますか　(y/n) ?"
-read ans
-if [ "$ans" = "y" ]; then
-	sudo pacman -S php php-intl
-	# already included
-	#sudo pacman -S php-cli 
-	#sudo pacman -S php-xml php-zip php-mbstring php-json php-curl
-
-	sudo pacman -S unzip wget
-	sudo pacman -S ffmpeg
-	#sudo pacman -S ffmpeg-devel
- 
-	sudo pacman -S at cronie
-	sudo pacman -S atomicparsley
-	sudo pacman -S chromium
- 
-	#sudo pacman -S samba
-	#sudo pacman -S lighttpd lighttpd-mod-webdav php-cgi
-	#sudo pacman -S libmp4v2
-	#sudo pacman -S gpac
-	#sudo pacman -S ImageMagick
-	#sudo pacman -S swftools
-
- 	sudo systemctl start atd
- 	sudo systemctl start atd cronie
-
-    	sudo ln -s /usr/bin/atomicparsley /usr/bin/AtomicParsley
-fi
-
-echo
-echo "rfriends3をインストールしますか　(y/n) ?"
-read ans
-if [ "$ans" = "y" ]; then
-    cd ~/
-    rm -f $SCRIPT
-    wget $SITE/$SCRIPT
-    unzip -q -o $SCRIPT
-fi
-# -----------------------------------------
-#echo rfriends3の実行方法
-#echo
-#echo cd ~/rfriends3
-#echo sh rfriends3.sh
-# -----------------------------------------
-#  ビルトインサーバ
+optlighttpd="off"
+optsamba="on"
+export optlighttpd
+export optsamba
 #
-echo
-echo rfriends3の実行方法
-echo 
-echo sh $HOME/rfriends3/rf3server.sh
-echo
-echo 以下が表示されるので、webブラウザでアクセス
-echo
-echo rfriends3_server start
-echo xxx.xxx.xxx.xxx:8000
-echo
+sh arch_install.sh 2>&1 | tee arch_install.log
 # -----------------------------------------
-# 終了
-# -----------------------------------------
+# finish
 echo
 echo finished
 # -----------------------------------------
